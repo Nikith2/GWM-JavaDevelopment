afdb978622cc47ba53a3629fed654147ac3ba13f
Nikith <111171016+Nikith2@users.noreply.github.com>
2023-08-15 19:53:05 +0530

    Update SimpleCurrencyConvertor.java

--- Currency Convertor/SimpleCurrencyConvertor.java
+++ Currency Convertor/SimpleCurrencyConvertor.java
@@ -1,31 +1,33 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
+//creating the class as SimpleCurrencyConverter
 public class SimpleCurrencyConverter {
     public static void main(String[] args) {
+        //using hash map for key value pairs.
         Map<String, Double> exchangeRates = new HashMap<>();
+        //taking the current exchange rates and storing it in key value pair.
         exchangeRates.put("USD", 1.0);
         exchangeRates.put("INR", 83.289);
         exchangeRates.put("EUR", 0.91);
         exchangeRates.put("GBP", 0.79);
         exchangeRates.put("JPY", 145.34);
 
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner obj = new Scanner(System.in);
+    //Taking input from the user using scanner class 
         System.out.println("Enter the amount: ");
-        double amount = scanner.nextDouble();
+        double amount = obj.nextDouble();
 
         System.out.print("Enter the source currency (Options: INR,USD,EUR,GBP,JPY): ");
-        String sourceCurrency = scanner.next().toUpperCase();
+        String sourceCurrency = obj.next().toUpperCase();
 
         System.out.print("Enter the target currency (Options: INR,USD,EUR,GBP,JPY): ");
-        String targetCurrency = scanner.next().toUpperCase();
-
+        String targetCurrency = obj.next().toUpperCase();
+        //checking the conditions 
         if (exchangeRates.containsKey(sourceCurrency) && exchangeRates.containsKey(targetCurrency)) {
             double sourceRate = exchangeRates.get(sourceCurrency);
             double targetRate = exchangeRates.get(targetCurrency);
-
+          //calculating the amount according to the exchanges rates
             double convertedAmount = (amount / sourceRate) * targetRate;
             System.out.println(amount + " " + sourceCurrency + " is equal to " + convertedAmount + " " + targetCurrency);
         } else {
